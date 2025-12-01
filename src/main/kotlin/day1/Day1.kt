fc3303dccbebf1c5238760094eaa691fbf4c1171
John Gatward <John.Gatward@sainsburys.co.uk>
2025-12-01 15:47:06 +0000

    day1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -4,45 +4,45 @@ import java.io.File
 
 class Day1 {
 
-    val lineRegex = Regex("""([LR])(\d+)""");
-
-    data class Instruction(val dir: String, val turns: Int)
-
     fun part1(file: String): Int {
-        var dialPos = 50;
-        var zeroCount = 0;
+        var dialPos = 50
+        var zeroCount = 0
         File(file).forEachLine { line ->
-            lineRegex.find(line)?.destructured?.let { (dir, mag) ->
-                dialPos = when (dir) {
-                    "R" -> (dialPos + mag.toInt()) % 100
-                    "L" -> (dialPos - mag.toInt() + 100) % 100
-                    else -> throw IllegalArgumentException("Invalid direction $dir")
-                };
-                if (dialPos == 0) zeroCount++;
-            };
+            val (dir, mag) = parseLine(line)
+            dialPos = when (dir) {
+                'R' -> (dialPos + mag) % 100
+                'L' -> (dialPos - mag + 100) % 100
+                else -> throw IllegalArgumentException("Invalid direction $dir")
+            }
+            if (dialPos == 0) zeroCount++
         }
-        return zeroCount;
+        return zeroCount
     }
 
     fun part2(file: String): Int {
-        var dialPos = 50;
-        var zeroCount = 0;
+        var dialPos = 50
+        var zeroCount = 0
 
         File(file).forEachLine { line ->
-            lineRegex.find(line)?.destructured?.let { (dir, magStr) ->
-                val mag = magStr.toInt();
-                val newDialPos: Int;
-                zeroCount += mag / 100;
-                if (dir == "R") {
-                    newDialPos = (dialPos + mag % 100) % 100;
-                    if (newDialPos < dialPos) zeroCount++;
-                } else {
-                    newDialPos = (dialPos - mag % 100 + 100) % 100;
-                    if ((newDialPos > dialPos || newDialPos == 0) && dialPos != 0) zeroCount++;
-                }
-                dialPos = newDialPos;
+            val (dir, mag) = parseLine(line)
+            val turns = mag % 100
+            val newDialPos: Int
+            zeroCount += mag / 100
+            if (dir == 'R') {
+                newDialPos = (dialPos + turns) % 100
+                if (newDialPos < dialPos) zeroCount++
+            } else {
+                newDialPos = (dialPos - turns + 100) % 100
+                if ((newDialPos > dialPos || newDialPos == 0) && dialPos != 0) zeroCount++
             }
+            dialPos = newDialPos
         }
-        return zeroCount;
+        return zeroCount
+    }
+
+    fun parseLine(line: String): Pair<Char, Int> {
+        val dir = line[0]
+        val mag = line.substring(1)
+        return Pair(dir, mag.toInt())
     }
 }
