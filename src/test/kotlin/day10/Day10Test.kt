40783cb3f3552a3ee2af6f18c7ca3d0d7444da1e
John Gatward <John.Gatward@sainsburys.co.uk>
2025-12-10 13:11:38 +0000

    day10p1

--- src/test/kotlin/day10/Day10Test.kt
+++ src/test/kotlin/day10/Day10Test.kt
@@ -1,30 +1,29 @@
-package day7
+package day10
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class Day7Test {
+class Day10Test {
 
     @ParameterizedTest
     @CsvSource(
-        "sample.txt, 21",
-        "input.txt, 1537"
+        "sample.txt, 7",
+        "input.txt, 479"
     )
     fun test_part1(file: String, expected: Int) {
-        val actual = Day7().part1(file)
+        val actual = Day10().part1(file)
 
         assertThat(actual).isEqualTo(expected)
     }
 
     @ParameterizedTest
     @CsvSource(
-        "sample.txt, 40",
-        "input.txt, 18818811755665",
+        "sample.txt, 0",
+        "input.txt, 0",
     )
-    fun test_part2(file: String, expected: Long) {
-        val actual = Day7().part2(file)
-
-        assertThat(actual).isEqualTo(expected)
+    fun test_part2(file: String, expected: Int) {
+        assertThrows<IllegalStateException> { Day10().part2(file) }
     }
 }
